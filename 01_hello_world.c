8b6761dcf56b9d7b476477202c0ff5defd76abc6
YimneRaid <43366463+YimneRaid@users.noreply.github.com>
2018-09-22 21:40:07 +0200

    Add files via upload

---
+++ 01_hello_world.c
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ----------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:		Yimne Raid
+ * ----------------------------------------------------------
+ * Description:
+ * "Hello Yimne Raid" is going to appear on the console
+ * ----------------------------------------------------------
+ */
+int main(int argc, char const *argv[]) {
+  printf("Hallo Yimne Raid \n");
+  return 0;
+}
